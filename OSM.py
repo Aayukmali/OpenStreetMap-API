d2a719869d339793708780f588dfce4e13d461f9
Janak Mali <83732486+Aayukmali@users.noreply.github.com>
2024-11-04 22:14:49 -0600

    Update OSM.py

--- OSM.py
+++ OSM.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import requests
 import geopandas as gpd
 import shapely
 from shapely.geometry import Point, Polygon, LineString
-
-
-# In[2]:
 
 
 overpass_url = 'https://overpass-api.de/api/interpreter'
@@ -31,14 +22,8 @@ response = requests.get(overpass_url,params = {'data':overpass_query})
 response
 
 
-# In[3]:
-
-
 data = response.json()
 datas = data['elements']
-
-
-# In[4]:
 
 
 ## Creating the nodes dataframe.
@@ -89,16 +74,9 @@ way_df['maxspeed'] = way_df['maxspeed'].str.replace('[a-zA-Z]', '', regex = True
 way_df['maxspeed'] =  pd.to_numeric(way_df['maxspeed'], errors = 'coerce').astype('Int64')
 
 
-
-# In[12]:
-
-
 ## Creating a geometry list.
 
 node_geometry = [Point(xy) for xy in zip(node_df.longitude,node_df.latitude)]
-
-
-# In[13]:
 
 
 ## Coordinate Reference System:
@@ -107,28 +85,14 @@ crs = "EPSG:3857"
 
 node_gdf = gpd.GeoDataFrame(node_df,crs = crs, geometry=node_geometry)
 
-
-# In[15]:
-
-
 node_gdf.plot()
-
-
-# In[16]:
-
 
 ## removing the records for railway speeds.
 way_df_filtered = way_df[way_df['railway']  != 'rail']
 exploded_df = way_df_filtered.explode('node_list')
 
 
-# In[17]:
-
-
 exploded_df['node_list'] = pd.to_numeric(exploded_df['node_list'], errors = 'coerce').astype('Int64')
-
-
-# In[18]:
 
 
 way_df_filtered
@@ -139,22 +103,12 @@ way_df_filtered
 osm_df = exploded_df.merge(node_df, left_on = 'node_list', right_on = 'node_id', how = 'inner')
 
 
-# In[20]:
-
-
-
 geometry = [Point(xy) for xy in zip(osm_df.longitude, osm_df.latitude)]
 osm_gdf = gpd.GeoDataFrame(osm_df, crs = "EPSG:3587", geometry = geometry)
 osm_gdf.head(2)
 
 
-# In[22]:
-
-
 osm_gdf.drop(['node_list', 'latitude','longitude', 'railway'], axis = 1, inplace = True)
-
-
-# In[24]:
 
 
 osm_gdf1 = osm_gdf.groupby(['way_id', 'highway', 'lanes', 'maxspeed', 'name', 'county', 'oneway',
@@ -162,23 +116,10 @@ osm_gdf1 = osm_gdf.groupby(['way_id', 'highway', 'lanes', 'maxspeed', 'name', 'c
 
 
 
-# In[25]:
-
-
 osm_gdf1['line'] = osm_gdf1.apply(lambda row: LineString(row['geometry']), axis = 1)
-
-
-# In[26]:
 
 
 osm_gdf2 = gpd.GeoDataFrame(osm_gdf1, crs = "EPSG:3857", geometry = osm_gdf1['line'])
 
 
 osm_gdf2.plot()
-
-
-# In[ ]:
-
-
-
-
